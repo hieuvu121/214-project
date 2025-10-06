2c0de87612104c9ca27a7531b83afe572d20b09b
hieu <hieubeobilabi@gmail.com>
2025-10-07 09:18:18 +1100

    Local changes before merge

--- airline/bookings/views.py
+++ airline/bookings/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic.base import TemplateView
 # Create your views here.
 def booking(request):
     return HttpResponse('hello')
+
+class homeView(TemplateView):
+    template_name='home.html'
